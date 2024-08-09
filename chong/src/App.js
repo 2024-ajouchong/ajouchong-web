ffb99188a70f7a49e55e888958d569a904ceb7e6
tae1231 <111653481+tae1231@users.noreply.github.com>
2024-08-09 15:50:45 +0900

    modified:app

--- chong/src/App.js
+++ chong/src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from "./components/Footer";
+import Main from "./pages/Main/index";
+
 
 function App() {
   return (
       <div className="App">
         <Header/>
+
+          <Main/>
+
+
           <Footer/>
       </div>
   );
