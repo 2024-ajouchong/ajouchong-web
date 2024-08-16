ed4fb065ee77916008238d2e4b9570b31c1e4b97
tae1231 <111653481+tae1231@users.noreply.github.com>
2024-08-17 01:02:49 +0900

    #9 feat:rental

--- chong/src/pages/Rental/index.js
+++ chong/src/pages/Rental/index.js
@@ -5,8 +5,15 @@ const Rental = () => {
     return (
         <div className="context">
             <div className="contextTitle">대여사업</div>
-            <hr className="titleSeparator" />
+            <hr className="titleSeparator"/>
+            <div className="rental-container">
+                <div className="rental-subTitle">대여사업 현황</div>
+                <div className="rental-box"></div>
+                <ul className="rental-button">
+                    <li><a href="#대여사업"> > 대여사업 바로가기 </a></li>
 
+                </ul>
+            </div>
         </div>
     );
 }
