1962fcb41cdddc46beae4ba8754f3b50dc5af043
tae1231 <111653481+tae1231@users.noreply.github.com>
2024-08-17 00:40:24 +0900

    #9 feat:audit

--- chong/src/pages/Audit/index.js
+++ chong/src/pages/Audit/index.js
@@ -5,8 +5,11 @@ const Audit = () => {
     return (
         <div className="context">
             <div className="contextTitle">감사자료</div>
-            <hr className="titleSeparator" />
+            <hr className="titleSeparator"/>
+            <ul className="audit-container">
+                <li><a href="#감사자료"> > 총학생회 학생회 감사자료 바로가기 </a></li>
 
+            </ul>
         </div>
     );
 }
--- chong/src/pages/Promotion/index.js
+++ chong/src/pages/Promotion/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Promotion = () => {
     return (
         <div className="context">
-            <div className="contextTitle">제휴사업</div>
+            <div className="contextTitle">제휴백과</div>
             <hr className="titleSeparator" />
 
         </div>
