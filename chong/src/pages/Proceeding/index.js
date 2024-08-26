722b26f0fb4cbc4448cb42ae19a060f7b7f3084f
tae1231 <111653481+tae1231@users.noreply.github.com>
2024-08-26 15:35:24 +0900

    feat: rental

--- chong/src/pages/Proceeding/index.js
+++ chong/src/pages/Proceeding/index.js
@@ -7,8 +7,8 @@ const Proceeding = () => {
             <div className="contextTitle">회의록</div>
             <hr className="titleSeparator"/>
             <ul className="proceeding-container">
-            <li><a href="#중앙위원회 회의"> > 중앙위원회 회의록 바로가기 </a></li>
-            <li><a href="#중앙공간위원회 회의"> > 중앙공간운영위원회 회의록 바로가기 </a></li>
+            <li><a href="https://drive.google.com/drive/folders/1C2W5ZUTJgJaZPIJiIqTOUYQTIJKIXPU0"> > 중앙위원회 회의록 바로가기 </a></li>
+            <li><a href="https://drive.google.com/drive/folders/1e8NACZyIYKgAbgNOgourBjTQwKngwZDo"> > 중앙공간운영위원회 회의록 바로가기 </a></li>
             </ul>
         </div>
     );
--- chong/src/pages/Rental/index.js
+++ chong/src/pages/Rental/index.js
@@ -7,12 +7,14 @@ const Rental = () => {
             <div className="contextTitle">대여사업</div>
             <hr className="titleSeparator"/>
             <div className="rental-container">
-                <div className="rental-subTitle">대여사업 현황</div>
-                <div className="rental-box"></div>
-                <ul className="rental-button">
-                    <li><a href="#대여사업"> > 대여사업 바로가기 </a></li>
+                <div className="rental-subTitle">물품 대여 현황</div>
+                {/*<div className="rental-box"></div>*/}
+                <iframe className="rental-box"
+                    src="https://docs.google.com/spreadsheets/d/1IU3CiAEz99-MzoRr-IVcihwIItuqx-H_2bsv7n_SZU8/pubhtml?widget=true&amp;headers=false"></iframe>
+                {/*<ul className="rental-button">*/}
+                {/*    <li><a href="#대여사업"> > 대여사업 바로가기 </a></li>*/}
 
-                </ul>
+                {/*</ul>*/}
             </div>
         </div>
     );
